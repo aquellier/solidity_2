7c34a7d3598ad2a516ddc851506a89f60d3d3ec0
aquellier <antoinecquellier@gmail.com>
2022-01-21 22:22:13 +0100

    add inbox file

---
+++ inbox/Inbox.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Inbox
+ * @dev Implements a simple inbox system
+ */
+contract Inbox {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
